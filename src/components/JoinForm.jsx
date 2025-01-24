4b8ea795d4393b4c65fcdba8f83eaf851028af24
Sumu Vuori <sumu.vuori@tuni.fi>
2025-01-25 01:27:31 +0200

    feat: use state arrays for questions

--- src/components/JoinForm.jsx
+++ src/components/JoinForm.jsx
@@ -1,28 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 function JoinForm() {
   const { t } = useTranslation();
   const tr = t("join-us", { returnObjects: true });
+  const [answers, setAnswers] = useState([]);
 
   useEffect(() => {
-    console.log(tr);
-  }, []);
+    if (!tr.questions) return;
+    const questionBase = tr.questions.map((question) => ({
+      ...question,
+      userInput: "",
+    }));
+    setAnswers(questionBase);
+  }, [tr]);
 
   return (
     <div>
       <h3>{tr.title}</h3>
 
-      {tr.questions && (
-        <ul>
-          {tr.questions.map((q, i) => (
-            <li key={`q${i}`}>
-              <label>{q.q}</label>
-              <input type="text" placeholder={q.a} />
-            </li>
-          ))}
-        </ul>
-      )}
+      <ul>
+        {answers.map((q, i) => (
+          <li key={`q${i}`}>
+            <label>{q.q}</label>
+            <input type="text" placeholder={q.a} />
+          </li>
+        ))}
+      </ul>
 
       <button onClick={() => console.log("click")}>Lähetä</button>
       <button onClick={() => console.log("click")}>Reset</button>
