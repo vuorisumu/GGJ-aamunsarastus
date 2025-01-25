b5cee6f7f9ddf5e8fee2c9938330a947664e98e5
Sumu Vuori <sumu.vuori@tuni.fi>
2025-01-25 20:51:27 +0200

    feat: remove unnecessary lines

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,10 @@
 import { useTranslation } from "react-i18next";
 import PageIntro from "./PageIntro";
 import Content from "./Content";
-import { useContext } from "react";
-import UserContext from "../util/UserContext";
 
 function About() {
   const { t } = useTranslation();
   const tr = t("about", { returnObjects: true });
-  const { phase } = useContext(UserContext);
 
   return (
     <div>
