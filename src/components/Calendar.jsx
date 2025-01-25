1c1b1574ac989ce6c4951fc8d685f085b153c070
Sumu Vuori <sumu.vuori@tuni.fi>
2025-01-25 19:35:55 +0200

    feat: input field to calendar

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,13 +1,22 @@
 import { useTranslation } from "react-i18next";
 import PageIntro from "./PageIntro";
+import { useState } from "react";
 
 function Calendar() {
   const { t } = useTranslation();
   const tr = t("calendar", { returnObjects: true });
+  const [input, setInput] = useState("");
 
   return (
     <div>
       <PageIntro data={tr} />
+
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={() => console.log(input)}>Lähetä</button>
     </div>
   );
 }
