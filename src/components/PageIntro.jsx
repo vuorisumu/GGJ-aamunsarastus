6fd115b11ae86948366d0420e45e7746b539f47a
Sumu Vuori <sumu.vuori@tuni.fi>
2025-01-25 14:04:40 +0200

    feat: banner texts on top of image

--- src/components/PageIntro.jsx
+++ src/components/PageIntro.jsx
@@ -2,8 +2,10 @@ function PageIntro({ data }) {
   return (
     <div className="banner">
       {data.img && <img src={`/images/${data.img}`} />}
-      {data.title && <h1>{data.title}</h1>}
-      {data.description && <p>{data.description}</p>}
+      <div className="texts">
+        {data.title && <h1>{data.title}</h1>}
+        {data.description && <p>{data.description}</p>}
+      </div>
     </div>
   );
 }
