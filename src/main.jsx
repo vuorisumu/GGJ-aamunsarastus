6ade2fd9fd7f05b965eb6d6d55ff67b35f5baf4a
Sumu Vuori <sumu.vuori@tuni.fi>
2025-01-25 14:10:33 +0200

    feat: use scss

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./styles/main.css";
 import "./util/i18n.js";
 import App from "./App.jsx";
 
