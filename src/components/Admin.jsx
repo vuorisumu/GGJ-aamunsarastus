9c10dfd21e2b75ef28b0552c7c2584ab6f8edf7a
Sumu Vuori <sumu.vuori@tuni.fi>
2025-01-26 01:12:43 +0200

    feat: remove colors from phase 3

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,10 +1,8 @@
 import { useTranslation } from "react-i18next";
 import PageIntro from "./PageIntro";
-import Content from "./Content";
 import { useContext, useEffect } from "react";
 import UserContext from "../util/UserContext";
 import { useNavigate } from "react-router-dom";
-import pentagram from "../../public/images/penta.png";
 
 function Admin() {
   const { phase } = useContext(UserContext);
@@ -22,10 +20,8 @@ function Admin() {
     <div>
       <PageIntro data={tr} />
       <div className="center-text">
-        <img src={pentagram} alt="pentagram" className="glow-image penta" />
         {tr.manifest && tr.manifest.map((m, i) => <p key={`m${i}`}>{m}</p>)}
       </div>
-      <Content data={tr.content} />
     </div>
   );
 }
