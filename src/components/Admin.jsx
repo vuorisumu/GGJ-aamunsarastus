58a4999a59c7beb608aaceafab36c0910c9fe64d
Patrick-Palmroos <palmroos.patrick@gmail.com>
2025-01-25 22:21:49 +0200

    Change: admin page to have horrific imagery.

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -4,6 +4,9 @@ import Content from "./Content";
 import { useContext, useEffect } from "react";
 import UserContext from "../util/UserContext";
 import { useNavigate } from "react-router-dom";
+import sac1 from "../../public/images/sacrifice.png";
+import sac2 from "../../public/images/sac2.jpg";
+import sac3 from "../../public/images/sac3.jpg";
 
 function Admin() {
     const { phase } = useContext(UserContext);
@@ -20,7 +23,23 @@ function Admin() {
     return (
         <div>
             <PageIntro data={tr} />
-
+            <div id="admins">
+                <section>
+                    <img src={sac1} alt="admin 1" />
+                    <h2>Admin 1</h2>
+                    <p> - dealt with</p>
+                </section>
+                <section>
+                    <img src={sac2} alt="admin 1" />
+                    <h2>Admin 2</h2>
+                    <p> - dealt with</p>
+                </section>
+                <section>
+                    <img src={sac3} alt="admin 1" />
+                    <h2>Admin 3</h2>
+                    <p> - dealt with</p>
+                </section>
+            </div>
             <Content data={tr.content} />
         </div>
     );
