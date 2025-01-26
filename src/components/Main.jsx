4ebedeac593a3dbffcdbaf108c89d3239d7e7fe4
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-01-26 03:05:43 +0200

    Add: video to main page.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,6 +4,7 @@ import Content from "./Content";
 import CommentSection from "./CommentSection";
 import { useContext } from "react";
 import UserContext from "../util/UserContext";
+import vid from "../../public/videos/ad_compr.mp4";
 
 function Main() {
   const { clearPhase } = useContext(UserContext);
@@ -13,7 +14,12 @@ function Main() {
   return (
     <div>
       <PageIntro data={tr} />
-
+      <div className="video-container">
+        <video controls width="600">
+          <source src={vid} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
       <Content data={tr.content} />
       <CommentSection data={tr.comments} />
 
