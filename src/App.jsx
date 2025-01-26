ee56fe1347f6efdc760255d0c13547cf91c9ddc6
Sumu Vuori <sumu.vuori@tuni.fi>
2025-01-26 13:09:15 +0200

    feat: increase time until popup

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
       const interval = setInterval(() => {
         if (popup === false) {
           const elapsedTime = new Date() - getStartTime();
-          if (elapsedTime > 5000) {
+          if (elapsedTime > 40000) {
             setPopup(true);
             setPopupStart(new Date());
           }
